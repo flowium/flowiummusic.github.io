1be11a81c05ef32f95da6faafe6948da6847a0d1
flowium <flowiummusic@gmail.com>
2020-05-11 00:14:57 +0300

    complete new landing page

--- dist/main.0d3aeac4.js
+++ dist/main.0d3aeac4.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../assets/img/icon-facebook-white.svg":[["icon-facebook-white.f1721ea7.svg","src/assets/img/icon-facebook-white.svg"],"src/assets/img/icon-facebook-white.svg"],"./../assets/img/icon-instagram-white.svg":[["icon-instagram-white.83ae3d03.svg","src/assets/img/icon-instagram-white.svg"],"src/assets/img/icon-instagram-white.svg"],"./../assets/img/icon-twitter-white.svg":[["icon-twitter-white.c6f4043a.svg","src/assets/img/icon-twitter-white.svg"],"src/assets/img/icon-twitter-white.svg"],"./../assets/img/icon-spotify-white.svg":[["icon-spotify-white.968ebb53.svg","src/assets/img/icon-spotify-white.svg"],"src/assets/img/icon-spotify-white.svg"],"./../assets/img/background-black.svg":[["background-black.17d75dce.svg","src/assets/img/background-black.svg"],"src/assets/img/background-black.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../assets/img/icon-facebook-white.svg":[["icon-facebook-white.f1721ea7.svg","src/assets/img/icon-facebook-white.svg"],"src/assets/img/icon-facebook-white.svg"],"./../assets/img/icon-instagram-white.svg":[["icon-instagram-white.83ae3d03.svg","src/assets/img/icon-instagram-white.svg"],"src/assets/img/icon-instagram-white.svg"],"./../assets/img/icon-twitter-white.svg":[["icon-twitter-white.c6f4043a.svg","src/assets/img/icon-twitter-white.svg"],"src/assets/img/icon-twitter-white.svg"],"./../assets/img/icon-spotify-white.svg":[["icon-spotify-white.968ebb53.svg","src/assets/img/icon-spotify-white.svg"],"src/assets/img/icon-spotify-white.svg"],"./../assets/img/background-black.svg":[["background-black.17d75dce.svg","src/assets/img/background-black.svg"],"src/assets/img/background-black.svg"],"./../assets/img/background-organic-gray.svg":[["background-organic-gray.1a4d42e8.svg","src/assets/img/background-organic-gray.svg"],"src/assets/img/background-organic-gray.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59578" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58610" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
